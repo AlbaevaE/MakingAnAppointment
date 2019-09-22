c46d8694aa46ff62066233abad3b49c436733388
Elvira <elvira.albaeva@gmail.com>
2019-09-22 17:32:23 +0600

    created MedicalHistory

---
+++ src/main/java/com/example/makingAnAppointment/model/MedicalHistory.java
@@ -0,0 +1,26 @@
+package com.example.makingAnAppointment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="medical_history")
+public class MedicalHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany
+    @JoinColumn(name="test_id")
+    private MedicalTests test;
+
+
+}
